2ce9bf6fd3ba8971d1ccfe0f986e908c5b2903a3
MooseValley <Moose_Software@yahoo.com.au>
2021-10-09 19:24:21 +1000

    Updates

--- php/personcheckin/Constants.php
+++ php/personcheckin/Constants.php
@@ -59,12 +59,8 @@ class Constants
       . '   pc.id as "Id" '
       . ' , pc.personName as "Person" '
       . ' , DATE_FORMAT(pc.checkinDateTime, "%a, %d-%b-%Y @ %l:%i:%s %p") as "Date/Time" '
-      . ' , FORMAT (TIMESTAMPDIFF(DAY,  pc.checkinDateTime, NOW() ), 1) as "Age (days)"  '
-    //. ' , FORMAT (1 + TIMESTAMPDIFF(DAY,  pc.checkinDateTime, NOW() ) +  '
-    //. ' , FORMAT (TIMESTAMPDIFF(HOUR, pc.checkinDateTime, NOW() ) / 24.0, 1) as "Age (days)" '
+      . ' , TIMESTAMPDIFF(DAY, NOW(), pc.checkinDateTime) as "Age (days)" '
     //. ' , FORMAT(TIMESTAMPDIFF(MICROSECOND, NOW(), pc.checkinDateTime) / 1000.0 / 60.0 / 60.0, 1) as "Age (hours)" '
-    //. ' , FORMAT ((NOW() - pc.checkinDateTime) / 60.0 / 60.0 / 24.0, 1) as "Age (days)" '
-    //. ' , FORMAT (DATEFIFF(NOW(), pc.checkinDateTime), 1) as "Age (days)" '
       . ' , pc.comments as "Comments" '
       . ' FROM  personCheckIn pc ';
 
