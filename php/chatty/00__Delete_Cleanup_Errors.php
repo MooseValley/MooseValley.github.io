96aa703bbb825d80b18155870c7f2d48a0ae58df
MooseValley <Moose_Software@yahoo.com.au>
2021-11-28 19:08:57 +1000

    8-Nov-2021
    8-Nov-2021 :: George Harrison: 'Do you want me to go with you ?'

--- php/chatty/00__Delete_Cleanup_Errors.php
+++ php/chatty/00__Delete_Cleanup_Errors.php
@@ -16,7 +16,13 @@ $db = new Database__MySQL();
 //     . ' ORDER BY id DESC ';
    //. ' LIMIT 5 '; // same as "SELECT TOP 5 ... " in SQL Server.
 
-$sql = ' DELETE FROM personCheckIn WHERE id = 141 ';
+//$sql = ' DELETE FROM personCheckIn WHERE id = 141 ';
+
+$sql = ' UPDATE personCheckIn '
+ . ' SET comments = '
+ . ' "Been watching some George Harrison interviews (and music) ....  While My Guitar Gently Wheeps, My Sweet Lord, tribute videos, ....  in one interview, Ringo tells the story of seeing George for the last time. George was terribly ill and in the hospital in Switzerland, bedridden. Ringo says to George, ""You know I love you, but I`ve got to go to Boston. My Daughter`s having surgery for a brain tumor.""  George replied, ""Do you want me to go with you?""  Humble, thoughtful, and always ready to put other people`s needs ahead of his own.  What a fucking top bloke !!!  The world needs more people like George.  Now more than ever.   Ref: <a target=_blank href=""https://www.youtube.com/watch?v=0LBJoj4Vyb0"">  George Harrison`s last words with Ringo Starr</a>" '
+ . ' WHERE id = 176 ';
+
 
 $results = $db->executeSQLQuery ($sql);
 
