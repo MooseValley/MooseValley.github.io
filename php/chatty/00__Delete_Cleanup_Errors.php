4886c06ba9b37490b06e94393f36338d25359460
MooseValley <Moose_Software@yahoo.com.au>
2021-11-28 21:33:26 +1000

    8-Nov-2021
    8-Nov-2021 :: George Harrison: 'Do you want me to go with you ?'

--- php/chatty/00__Delete_Cleanup_Errors.php
+++ php/chatty/00__Delete_Cleanup_Errors.php
@@ -20,7 +20,7 @@ $db = new Database__MySQL();
 
 $sql = ' UPDATE personCheckIn '
  . ' SET comments = '
- . ' "Been watching some George Harrison interviews (and music) ....  While My Guitar Gently Wheeps, My Sweet Lord, tribute videos, ....  in one interview, Ringo tells the story of seeing George for the last time. George was terribly ill and in the hospital in Switzerland, bedridden. Ringo says to George, ""You know I love you, but I`ve got to go to Boston. My Daughter`s having surgery for a brain tumor.""  George replied, ""Do you want me to go with you?""  Humble, thoughtful, and always ready to put other people`s needs ahead of his own.  What a fucking top bloke !!!  The world needs more people like George.  Now more than ever.   Ref: <a target=_blank href=""https://www.youtube.com/watch?v=0LBJoj4Vyb0"">  George Harrison`s last words with Ringo Starr</a>" '
+ . ' "Been watching some George Harrison interviews (and music) ....  While My Guitar Gently Weeps, My Sweet Lord, tribute videos, ....  in one interview, Ringo tells the story of seeing George for the last time. George was terribly ill and in the hospital in Switzerland, bedridden. Ringo says to George, ""You know I love you, but I`ve got to go to Boston. My Daughter`s having surgery for a brain tumor.""  George replied, ""Do you want me to go with you?""  Humble, thoughtful, and always ready to put other people`s needs ahead of his own.  What a fucking top bloke !!!  The world needs more people like George.  Now more than ever.   Ref: <a target=_blank href=""https://www.youtube.com/watch?v=0LBJoj4Vyb0"">  George Harrison`s last words with Ringo Starr</a>" '
  . ' WHERE id = 176 ';
 
 
