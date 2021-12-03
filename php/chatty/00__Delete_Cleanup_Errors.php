755cb9a0354eebac6766b2731d1f60ec8d3fcbad
MooseValley <Moose_Software@yahoo.com.au>
2021-12-03 12:25:03 +1000

    Updates

--- php/chatty/00__Delete_Cleanup_Errors.php
+++ php/chatty/00__Delete_Cleanup_Errors.php
@@ -18,10 +18,16 @@ $db = new Database__MySQL();
 
 //$sql = ' DELETE FROM personCheckIn WHERE id = 141 ';
 
+//$sql = ' UPDATE personCheckIn '
+// . ' SET comments = '
+// . ' "Been watching some George Harrison interviews (and music) ....  While My Guitar Gently Weeps, My Sweet Lord, tribute videos, ....  in one interview, Ringo tells the story of seeing George for the last time. George was terribly ill and in the hospital in Switzerland, bedridden. Ringo says to George, ""You know I love you, but I`ve got to go to Boston. My Daughter`s having surgery for a brain tumor.""  George replied, ""Do you want me to go with you?""  Humble, thoughtful, and always ready to put other people`s needs ahead of his own.  What a fucking top bloke !!!  The world needs more people like George.  Now more than ever.   Ref: <a target=_blank href=""https://www.youtube.com/watch?v=0LBJoj4Vyb0"">  George Harrison`s last words with Ringo Starr</a>" '
+// . ' WHERE id = 176 ';
+
 $sql = ' UPDATE personCheckIn '
  . ' SET comments = '
- . ' "Been watching some George Harrison interviews (and music) ....  While My Guitar Gently Weeps, My Sweet Lord, tribute videos, ....  in one interview, Ringo tells the story of seeing George for the last time. George was terribly ill and in the hospital in Switzerland, bedridden. Ringo says to George, ""You know I love you, but I`ve got to go to Boston. My Daughter`s having surgery for a brain tumor.""  George replied, ""Do you want me to go with you?""  Humble, thoughtful, and always ready to put other people`s needs ahead of his own.  What a fucking top bloke !!!  The world needs more people like George.  Now more than ever.   Ref: <a target=_blank href=""https://www.youtube.com/watch?v=0LBJoj4Vyb0"">  George Harrison`s last words with Ringo Starr</a>" '
- . ' WHERE id = 176 ';
+ . ' "Clayton Rd and Hawke St going on the market. Both of these properties are being sold. Motel house also going to be sold as soon as Murray finishes moving out. Pretty soon all I will own is my farm.  My Rates bill will go from $42K+ pa. down to about $4K.  No more will rates be "bleeding me dry", as stupid, greedy, incompetent local councils keep jacking up rates like there is no tomorrow, even when property prices are falling." '
+ . ' WHERE id = 186 ';
+
 
 
 $results = $db->executeSQLQuery ($sql);
